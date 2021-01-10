a07479fa7ff1ed805a4a13138235ec6e72fa212e
J-Mara <jmaragakis20@stuy.edu>
2021-01-10 11:37:05 -0500

    linked list method adding

---
+++ MyLinkedList.java
@@ -0,0 +1,40 @@
+public class MyLinkedList{
+ private int size;
+ private Node start,end;
+ public MyLinkedList(){
+   size = 0;
+ }
+ public int size(){
+   return size;
+ }
+ public boolean add(String value){
+   Node newNode = new Node(value);
+   if(size == 0){
+     start = end = newNode;
+     size++;
+   }else{
+     Node holder = end;
+     end.setNext(newNode);
+     end = newNode;
+     newNode.setNext(null);
+     size++;
+     }
+     return true;
+  }
+// public void add(int index, String value);
+ public String get(int index){
+   Node counter = start;
+   for(int i = 0; i < size; i++){
+     if(i == index){
+       return counter.getData();
+     }
+     else{
+       counter = counter.getNext();
+     }
+  }
+  return "There is no value at that index";
+}
+}
+//  public String set(int index, String value);
+//  public String toString();
+//  //Any helper method that returns a Node object MUST BE PRIVATE!
